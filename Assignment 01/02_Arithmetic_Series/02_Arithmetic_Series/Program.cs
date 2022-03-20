152515113b1ede1e314d9a17f65b006d969e5047
Chaitanya Khairmode <chaitanyakhairmode912@gmail.com>
2022-03-20 23:58:55 +0530

    Assignment No 1

---
+++ Assignment 01/02_Arithmetic_Series/02_Arithmetic_Series/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02_Arithmetic_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A1, A2, N;
+
+            UP:
+            Console.Write("Enter First No :- ");
+            A1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second No :- ");
+            A2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter The Nth No :- ");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            if (A1 == 0 && A2 == 0)
+            {
+                Console.Write("Plz Enter Valid No...!! \n\n");
+                goto UP;
+            }
+
+            int D = A2 - A1;
+
+            N = A1 + ((N - 1) * D);
+
+            Console.Write(N);
+            Console.ReadKey();
+        }
+    }
+}
