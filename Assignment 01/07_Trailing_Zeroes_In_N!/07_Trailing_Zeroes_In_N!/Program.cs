4ef84e6cd25973ef78f0039e6928ec8e04c36fd6
Chaitanya Khairmode <chaitanyakhairmode912@gmail.com>
2022-03-21 12:26:45 +0530

    Assignment No 1

---
+++ Assignment 01/07_Trailing_Zeroes_In_N!/07_Trailing_Zeroes_In_N!/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _07_Trailing_Zeroes_In_N_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0 , fact = 1;
+
+            Up:
+            Console.Write(" Enter A Number :- ");
+            N = Convert.ToInt32(Console.ReadLine());
+            
+            if (N <= 0)
+            {
+                Console.Write(" Invalid No Plz Enter Again No");
+                goto Up;
+            }
+
+            for (; N > 0 ; N-- )
+            {
+                fact *= N; 
+            }
+
+            if (fact < 0)
+            {
+                fact = -(fact);
+            }
+
+            Console.Write("\n Factorial Of Given Number is :- "+fact + "\n\n");
+
+            Console.Write(" Trailing Zero of Given Factorial is :- " + CntOfZero(fact));
+            Console.ReadKey();
+        }
+
+        static int CntOfZero(int Num)
+        {
+            int Cnt = 0;
+
+            for (; Num > 0;Num /=10 )
+            {
+                if (Num%10 == 0)
+                {
+                    Cnt++;
+                }
+            }
+            return Cnt;
+        }
+    }
+}
