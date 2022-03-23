d346f6fed4c759b2a398d30d15ea1d2d53e7b008
Chaitanya Khairmode <chaitanyakhairmode912@gmail.com>
2022-03-24 00:42:42 +0530

    Assignment No 1

---
+++ Assignment 01/01_Pattern_From_Given_No/01_Pattern_From_Given_No/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01_Pattern_From_Given_No
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0;
+
+            //Accept the Number From User To Print The Given Pattern
+
+            Console.Write("Enter a Number For Print The Pattern : ");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n_______________Given Pattern Is_______________________\n\n");
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = N; j > 0; j--)
+                {
+                    for (int k = N; k > i; k--)
+                    {
+                        Console.Write(j);
+
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write("_______________________________________________________");
+
+            Console.ReadKey();
+        }
+    }
+}
